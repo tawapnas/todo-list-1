d34df454bae2c6d6d855daf6eeda0761002f7f6a
tawapnas <koyong555@hotmail.com>
2024-06-05 19:43:16 +0700

    Initial Commit

---
+++ todo-list-1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  todo-list-1
+//
+//  Created by Sanpawat Sewsuwan on 5/6/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ todo-list-1/todo_list_1App.swift
@@ -0,0 +1,17 @@
+//
+//  todo_list_1App.swift
+//  todo-list-1
+//
+//  Created by Sanpawat Sewsuwan on 5/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct todo_list_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
