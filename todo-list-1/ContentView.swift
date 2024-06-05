262175b4aa6b8174d6cad2de872bf2f8c18799bf
tawapnas <koyong555@hotmail.com>
2024-06-05 22:38:14 +0700

    update hello from develop

--- todo-list-1/ContentView.swift
+++ todo-list-1/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, Git!")
+            Text("Hello from Develop")
         }
         .padding()
     }
